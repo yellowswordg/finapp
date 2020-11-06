ff14b057ec48d076ceb8c28f57999aa6998d7408
Nikita Kozlov <yellowswordg@gmail.com>
2020-11-06 16:15:03 +0300

    refactor db to use singleton pattern

--- lib/repositories/dict/base_dict_repository.dart
+++ lib/repositories/dict/base_dict_repository.dart
@@ -1,20 +1,36 @@
 import 'dart:io';
 
-import 'package:finapp/models/word.dart';
 import 'package:finapp/repositories/base_repository.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-abstract class BaseDictRepository extends BaseRepository {
+class BaseDictRepository extends BaseRepository {
   static const String DB_NAME = 'dict.db';
 
-  Future<Database> database() async {
+  // Singleton patern db implementation
+  //we'll only have one instance of db
+  static final BaseDictRepository _instance = BaseDictRepository._();
+  static Database _database;
+  BaseDictRepository._();
+
+  factory BaseDictRepository() {
+    return _instance;
+  }
+
+  Future<Database> get db async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await init();
+    return _database;
+  }
+
+  Future<Database> init() async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, DB_NAME);
 
     final exists = await databaseExists(path);
-
     if (!exists) {
       // print("Creating new copy from asset");
 
@@ -35,6 +51,37 @@ abstract class BaseDictRepository extends BaseRepository {
     );
   }
 
-  Future<List<Word>> findInlanguage(
-      {String search, String language, String translation});
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  // Future<Database> database() async {
+  //   final dbPath = await getDatabasesPath();
+  //   final path = join(dbPath, DB_NAME);
+
+  //   final exists = await databaseExists(path);
+
+  //   if (!exists) {
+  //     // print("Creating new copy from asset");
+
+  //     try {
+  //       await Directory(dirname(path)).create(recursive: true);
+  //     } catch (_) {}
+
+  //     ByteData data = await rootBundle.load(join('assets/db/', DB_NAME));
+  //     List<int> bytes =
+  //         data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+  //     await File(path).writeAsBytes(bytes, flush: true);
+  //   } else {
+  //     // print('opening existing one');
+  //   }
+
+  //   return await openDatabase(
+  //     join(dbPath, DB_NAME),
+  //   );
+  // }
+
+  // Future<List<Word>> findInlanguage(
+  //     {String search, String language, String translation});
 }
--- lib/repositories/dict/dict_repository.dart
+++ lib/repositories/dict/dict_repository.dart
@@ -1,13 +1,14 @@
 import 'package:finapp/entities/word_entity.dart';
 
 import 'package:finapp/models/word.dart';
+import 'package:finapp/repositories/base_repository.dart';
 import 'package:finapp/repositories/dict/base_dict_repository.dart';
 
-class DictRepository extends BaseDictRepository {
+class DictRepository {
   Future<List<Word>> findInlanguage(
       {String search, String language, String translation}) async {
     // print("from repo translation:  ${translation} \n language:  ${language}");
-    final db = await database();
+    final db = await BaseDictRepository().db;
     List<Map> data = await db
         .rawQuery("SELECT * FROM data WHERE $language LIKE '%$search%'");
 
@@ -17,7 +18,4 @@ class DictRepository extends BaseDictRepository {
 
     return words;
   }
-
-  @override
-  void dispose() {}
 }
